4dd34918bfa99afc14bcf028b078e4a5ec50ef4d
David Byrne <David.dbyrne@gmail.com>
2016-05-14 18:26:36 +0100

    Completed lesson 2
    Now uses actual data from the Open Weather Map API

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -54,34 +44,5 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String forcastData[] = {
-                    "Sunny, 20*",
-                    "Cloudy, 21*",
-                    "Rainy, 18*",
-                    "Snowy, 2*"
-            };
-
-            List<String> weatherData = new ArrayList<String>(
-                    Arrays.asList(forcastData)
-            );
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>( getActivity(),
-                    R.layout.list_item_layout, R.id.list_item_forecast_textview, weatherData);
-
-            ListView lview = (ListView) rootView.findViewById(R.id.listview_forecast);
-            lview.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
